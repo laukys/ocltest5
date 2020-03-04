ae7017eebc88fefa524a33971500b6fce60c3963
laukys <idomjak@gmail.com>
2020-03-04 23:18:41 +0000

    change gpu

--- main.c
+++ main.c
@@ -39,12 +39,12 @@ int main(void) {
     //cl_device_id device_id = NULL;   
     cl_device_id device_id; 
     int err = clGetDeviceIDs(NULL, CL_DEVICE_TYPE_GPU, 1, &device_id, NULL);
-    cl_uint ret_num_devices;
+    /* cl_uint ret_num_devices;
     cl_uint ret_num_platforms;
     cl_int ret = clGetPlatformIDs(1, &platform_id, &ret_num_platforms);
     ret = clGetDeviceIDs( platform_id, CL_DEVICE_TYPE_ALL, 1, 
             &device_id, &ret_num_devices);
-
+	*/
     // Create an OpenCL context
     
     cl_context context = clCreateContext(0, 1, &device_id, NULL, NULL, &err);
@@ -52,7 +52,7 @@ int main(void) {
     // cl_context context = clCreateContext( NULL, 1, &device_id, NULL, NULL, &ret);
 
     // Create a command queue
-    cl_command_queue command_queue = clCreateCommandQueue(context, device_id, 0, &ret);
+    cl_command_queue command_queue = clCreateCommandQueue(context, device_id, 0, &err);
 
     // Create memory buffers on the device for each vector 
     cl_mem a_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY, 
