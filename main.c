f885f955bce6e0f9fcad8621073ec987dbe7bd96
laukys <idomjak@gmail.com>
2020-03-05 00:02:00 +0000

    change gpu

--- main.c
+++ main.c
@@ -36,20 +36,21 @@ int main(void) {
 
     // Get platform and device information
     cl_platform_id platform_id = NULL;
-    //cl_device_id device_id = NULL;   
-    cl_device_id device_id; 
-    cl_int ret = clGetDeviceIDs(NULL, CL_DEVICE_TYPE_GPU, 1, &device_id, NULL);
-    /* cl_uint ret_num_devices;
+    cl_device_id device_id = NULL; 
+    /*
+    cl_uint ret_num_devices;
     cl_uint ret_num_platforms;
     cl_int ret = clGetPlatformIDs(1, &platform_id, &ret_num_platforms);
     ret = clGetDeviceIDs( platform_id, CL_DEVICE_TYPE_ALL, 1, 
             &device_id, &ret_num_devices);
 	*/
+    
+    cl_int ret = clGetDeviceIDs(NULL, CL_DEVICE_TYPE_GPU, 1, &device_id, NULL);
+    
     // Create an OpenCL context
     
     cl_context context = clCreateContext(0, 1, &device_id, NULL, NULL, &err);
-
-    // cl_context context = clCreateContext( NULL, 1, &device_id, NULL, NULL, &ret);
+    //cl_context context = clCreateContext( NULL, 1, &device_id, NULL, NULL, &ret);
 
     // Create a command queue
     cl_command_queue command_queue = clCreateCommandQueue(context, device_id, 0, &ret);
