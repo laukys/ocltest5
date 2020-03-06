c371ecd13f01bb291afd975211c182051441d9cb
laukys <idomjak@gmail.com>
2020-03-06 14:04:20 +0000

    test5

--- main.c
+++ main.c
@@ -12,12 +12,14 @@
 int main(void) {
     // Create the two input vectors
     int i;
-    const int LIST_SIZE = 1024;
-    int *A = (int*)malloc(sizeof(int)*LIST_SIZE);
-    int *B = (int*)malloc(sizeof(int)*LIST_SIZE);
+    const int LIST_SIZE = 302*302*82;
+    float *A = (float*)malloc(sizeof(float)*LIST_SIZE);
+    float *B = (float*)malloc(sizeof(float)*LIST_SIZE);
     for(i = 0; i < LIST_SIZE; i++) {
-        A[i] = i;
-        B[i] = LIST_SIZE - i;
+        A[i] = rand()*1.0/10; 
+    }
+    for(i = 1; i < LIST_SIZE; i++) {
+        B[i] = A[i-1];
     }
 
     // Load the kernel source code into the array source_str
@@ -25,7 +27,7 @@ int main(void) {
     char *source_str;
     size_t source_size;
 
-    fp = fopen("vector_add_kernel.cl", "r");
+    fp = fopen("adam_map_36.cl", "r");
     if (!fp) {
         fprintf(stderr, "Failed to load kernel.\n");
         exit(1);
@@ -56,18 +58,16 @@ int main(void) {
     cl_command_queue command_queue = clCreateCommandQueue(context, device_id, 0, &ret);
 
     // Create memory buffers on the device for each vector 
-    cl_mem a_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY, 
-            LIST_SIZE * sizeof(int), NULL, &ret);
-    cl_mem b_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY,
-            LIST_SIZE * sizeof(int), NULL, &ret);
-    cl_mem c_mem_obj = clCreateBuffer(context, CL_MEM_WRITE_ONLY, 
-            LIST_SIZE * sizeof(int), NULL, &ret);
+    cl_mem a_mem_obj = clCreateBuffer(context,  CL_MEM_READ_WRITE , 
+            LIST_SIZE * sizeof(float), NULL, &ret);
+    cl_mem b_mem_obj = clCreateBuffer(context,  CL_MEM_READ_WRITE ,
+            LIST_SIZE * sizeof(float), NULL, &ret);
 
     // Copy the lists A and B to their respective memory buffers
     ret = clEnqueueWriteBuffer(command_queue, a_mem_obj, CL_TRUE, 0,
-            LIST_SIZE * sizeof(int), A, 0, NULL, NULL);
+            LIST_SIZE * sizeof(float), A, 0, NULL, NULL);
     ret = clEnqueueWriteBuffer(command_queue, b_mem_obj, CL_TRUE, 0, 
-            LIST_SIZE * sizeof(int), B, 0, NULL, NULL);
+            LIST_SIZE * sizeof(float), B, 0, NULL, NULL);
 
     // Create a program from the kernel source
     cl_program program = clCreateProgramWithSource(context, 1, 
@@ -77,27 +77,25 @@ int main(void) {
     ret = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
 
     // Create the OpenCL kernel
-    cl_kernel kernel = clCreateKernel(program, "vector_add", &ret);
+    cl_kernel kernel = clCreateKernel(program, "adam_map_36", &ret);
 
     // Set the arguments of the kernel
     ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&a_mem_obj);
     ret = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)&b_mem_obj);
-    ret = clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *)&c_mem_obj);
     
     // Execute the OpenCL kernel on the list
     size_t global_item_size = LIST_SIZE; // Process the entire lists
-    size_t local_item_size = 64; // Process in groups of 64
+    size_t local_item_size = 3000; // Process in groups of 64
     ret = clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL, 
             &global_item_size, &local_item_size, 0, NULL, NULL);
 
     // Read the memory buffer C on the device to the local variable C
-    int *C = (int*)malloc(sizeof(int)*LIST_SIZE);
-    ret = clEnqueueReadBuffer(command_queue, c_mem_obj, CL_TRUE, 0, 
-            LIST_SIZE * sizeof(int), C, 0, NULL, NULL);
+    ret = clEnqueueReadBuffer(command_queue, a_mem_obj, CL_TRUE, 0, 
+            LIST_SIZE * sizeof(float), A, 0, NULL, NULL);
 
     // Display the result to the screen
     for(i = 0; i < LIST_SIZE; i++)
-        printf("%d + %d = %d\n", A[i], B[i], C[i]);
+        printf("%d + %d = %d\n", A[i]);
 
     // Clean up
     ret = clFlush(command_queue);
@@ -106,12 +104,10 @@ int main(void) {
     ret = clReleaseProgram(program);
     ret = clReleaseMemObject(a_mem_obj);
     ret = clReleaseMemObject(b_mem_obj);
-    ret = clReleaseMemObject(c_mem_obj);
     ret = clReleaseCommandQueue(command_queue);
     ret = clReleaseContext(context);
     free(A);
     free(B);
-    free(C);
     return 0;
 }
 
